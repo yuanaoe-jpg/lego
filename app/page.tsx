b361b907d51ffa03a4430c6eb0d0c69302ef1472
alex <yuanaoe@gmail.com>
2025-08-06 20:32:09 +0800

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div className="bg-yellow-100 h-screen flex justify-center items-center ">
-  <div className="text-black">Hellow i am allen hellow charles Aikgiteng kenrick lirong  </div>
+  <div className="text-black">Hellow i am allen hellow charles Aikteng kenrick lirong  </div>
   </div>
   );
 }
