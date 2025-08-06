e75b04be5844e6c41deb1a7a6be914cdbeeb659c
alex <yuanaoe@gmail.com>
2025-08-06 20:36:55 +0800

    'ok'

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div className="bg-yellow-100 h-screen flex justify-center items-center ">
-  <div className="text-black">Hellow i am allen hellow charles Aikteng kenrick lirong  </div>
+  <div className="text-black">Hellow i am allen , who kill Aik Teng can get 1 doller  </div>
   </div>
   );
 }
